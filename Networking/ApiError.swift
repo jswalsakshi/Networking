53da6ac93b5112211662a65d12daf79ba5ed06cf
jswalsakshi <my@pers.on.al>
2024-02-10 19:55:48 +0530

    podspec setup

---
+++ Networking/ApiError.swift
@@ -0,0 +1,32 @@
+//
+//  ApiError.swift
+//  Networking
+//
+//  Created by sakshi.j on 10/02/24.
+//
+
+import Foundation
+
+enum ApiError: Error {
+    case requestFailed(description: String)
+    case invalidData
+    case invalidUrl
+    case responseUnSuccessful(description: String)
+    case jsonConversionfailure(description: String)
+    case jsonParsingFailure
+    case failedSerialization
+    case noInternet
+    
+    var customDescription: String {
+        switch self {
+        case let .requestFailed(description): return "Request Failed: \(description)"
+        case .invalidData: return "Invalid data"
+        case .invalidUrl: return "Invalid URL"
+        case let .responseUnSuccessful(description): return "Response Unsuccessful \(description)"
+        case let .jsonConversionfailure(description): return "Json Conversion failure \(description)"
+        case .jsonParsingFailure: return "JSON Parsing Failed"
+        case .failedSerialization: return "Failed Parsing"
+        case .noInternet: return "NO Internet Connection"
+        }
+    }
+}
